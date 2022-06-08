2914dede81dc5badea84bc7dd0848f1a777a3e73
Dylan Wang <wyc6990906@hotmail.com>
2022-06-08 10:26:36 -0600

    trying command move up W02D03

--- snakeGame/snake-client/client.js
+++ snakeGame/snake-client/client.js
@@ -4,17 +4,34 @@ const connect = function () {
     host: "10.0.2.15",
     port: 50541
   })
-  //Print a message to the screen when the connection is successfully established.
-  //seems not correct~
-  conn.on("data", (data) => {
-    console.log(`Name: DYL`)
-  })
-  conn.on("connect", () => {
-    console.log(`Successfully connected to game server...`)
-  })
-
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  //Print a message to the screen when the connection is successfully established.
+  // //seems not correct~
+  // conn.on("data", (data) => {
+  //   console.log(`Name: DYL`)
+  // })
+  conn.on("connect", () => {
+    console.log(`Successfully connected to game server...`)
+    const name = 'DYN'
+    conn.write(`Name: ${name}`);
+    /*
+    "Move: up" - move up one square (unless facing down)
+    "Move: down" - move down one square (unless facing up)
+    "Move: left" - move left one square (unless facing right)
+    "Move: right" - move left one square (unless facing left)
+    * */
+    conn.write('Move: up')
+    conn.write('Move: up')
+    conn.write('Move: up')
+    conn.write('Move: up')
+    conn.write('Move: up')
+    conn.write('Move: up')
+    conn.write('Move: up')
+    conn.write('Move: up')
+    conn.write('Move: up')
+  })
+
   return conn
 }
 
