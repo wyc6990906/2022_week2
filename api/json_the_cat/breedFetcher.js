0c65d19c69927b81a981cf432492be515250da1a
Dylan Wang <wyc6990906@hotmail.com>
2022-06-08 20:59:59 -0600

    breedFetcher W02D04

---
+++ api/json_the_cat/breedFetcher.js
@@ -0,0 +1,25 @@
+const request = require('request')
+const query = process.argv[2]
+
+request(`https://api.thecatapi11111.com/v1/breeds/search?q=${query}`, (error, response, body) => {
+  if (!error) {
+    // console.log('statusCode:', response && response.statusCode)
+    // console.log(typeof body);
+    const data = JSON.parse(body)
+    // console.log(data)
+    let description
+    if(error){
+      console.log('Something wrong: ',error.message)
+    }
+    if (data.length !== 0) {
+      description = data[0].description
+      console.log('description ==== ', description)
+    }
+    if(data.length ===0 ){
+      console.log('We cannot find any cat !')
+    }
+  }
+
+
+});
+
